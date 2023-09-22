9c7dc510855efdcde746246d005c785a4ce4325b
Timur <tiyalcin@gmail.com>
2023-09-22 12:11:34 +0200

    toggle rows and columns

--- app/javascript/components/gallery.js
+++ app/javascript/components/gallery.js
@@ -7,10 +7,10 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import h from "components/htm_create_element";
 
-export default function Gallery({ photos }) {
+export default function Gallery({ photos, layout }) {
   const [index, setIndex] = useState(-1);
 
-  return h`<${PhotoAlbum} layout="rows" spacing="5" photos="${photos}" onClick=${({
+  return h`<${PhotoAlbum} layout="${layout}" spacing="5" photos="${photos}" onClick=${({
     index,
   }) => setIndex(index)}  />
     <${Lightbox}
--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,15 +1,24 @@
 import { render } from "react-dom";
+import { useState } from "react";
 import h from "components/htm_create_element";
-
 import Gallery from "components/gallery";
 
-document.addEventListener("DOMContentLoaded", () => {
+export default function Wrapper() {
+  const [layout, setLayout] = useState("rows");
   const node = document.getElementById("photos");
   const photos = JSON.parse(node.getAttribute("data"));
 
-  render(
-    h`<${Gallery} photos=${photos}/>
-      <div>1</div>`,
-    document.getElementById("root")
-  );
+  const handleClick = () => {
+    if (layout === "rows") {
+      setLayout("columns");
+    } else {
+      setLayout("rows");
+    }
+  };
+
+  return h`<button class="btn btn-blue" onClick="${handleClick}">Change Layout</button><${Gallery} photos=${photos} layout="${layout}"/><div>1</div>`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(h`<${Wrapper} />`, document.getElementById("root"));
 });
