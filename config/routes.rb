4994b2c5bf9a7ce645c84e520539e9810c79aa7c
Timur <tiyalcin@gmail.com>
2023-10-10 10:24:01 +0200

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :pictures
+  resources :users
   resources :dashboard, only: [:index]
+  post '/dashboard', to: 'dashboard#create'
+
   resources :team, only: [:index]
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_09_22_145808) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_09_110756) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,6 +41,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_09_22_145808) do
 
   create_table "pictures", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.date "birthdate"
+    t.decimal "income", precision: 2, scale: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
