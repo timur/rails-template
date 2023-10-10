3999e4899ae147a75b02e0959b4f7289227ec1e5
Timur <tiyalcin@gmail.com>
2023-10-10 15:39:23 +0200

    input text hint error handling

--- app/helpers/components/input_helper.rb
+++ app/helpers/components/input_helper.rb
@@ -1,13 +1,7 @@
 module Components::InputHelper
   def render_input(name:, label: false, id: nil, type: :text, value: nil, **options)
-    # options[:class] = "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-colors ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 #{options[:class]} "
-    # options[:class] << case options[:variant]
-    # when :borderless
-    #   " border-0 focus-visible:outline-none focus-visible:shadow-none focus-visible:ring-transparent"
-    # else
-    #   "shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:border-muted"
-    # end
-    options[:class] = tw(options[:class])
+    base_classes = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+    input_classes = tw(base_classes, options[:class])
 
     options.reverse_merge!(
       label: (options[:label] || false),
@@ -18,7 +12,8 @@ module Components::InputHelper
       autocomplete: (options[:autocomplete] || ""),
       autocapitalize: (options[:autocapitalize] || nil),
       autocorrect: (options[:autocorrect] || nil),
-      autofocus: (options[:autofocus] || nil)
+      autofocus: (options[:autofocus] || nil),
+      hint: (options[:hint] || "")
     )
     render partial: "components/ui/input", locals: {
       type:,
@@ -26,6 +21,7 @@ module Components::InputHelper
       name:,
       value:,
       id:,
+      input_classes:,
       options: options
     }
   end
