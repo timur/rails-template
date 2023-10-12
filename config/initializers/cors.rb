b3d86117b4c8f30ee48a4b51b6faf972305512c2
Timur <tiyalcin@gmail.com>
2023-10-12 13:00:44 +0200

    enable all cors has to be made better

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '/map', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
