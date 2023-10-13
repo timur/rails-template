b1e2a8632506ced09def281e87a3a34e14f28a95
Timur <tiyalcin@gmail.com>
2023-10-13 14:27:29 +0200

    add rounded buttons

--- app/helpers/components/button_helper.rb
+++ app/helpers/components/button_helper.rb
@@ -1,109 +1,69 @@
 module Components::ButtonHelper
-
-  def render_primary_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
-    button_classes = "rounded bg-primary-600 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+  def render_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, button_classes: "", **options, &block)
+    base_classes = "rounded text-xs font-semibold text-white shadow-sm hover:bg-{{bg}} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{{bg}}"
+    
     variant_classes = case variant.to_sym
-    when :lg
-      "px-2.5 py-1.5"
     when :sm
-      "px-2 py-1 text-xs"
+      "px-2 py-1"
     when :md
-      "rounded-md px-2 py-1"
+      "px-2 py-1 text-sm"
+    when :lg
+      "px-2.5 py-1.5 text-sm"
     when :xl
-      "rounded-md px-3 py-2"
+      "px-3 py-2 text-sm"
     when :xxl
-      "rounded-md px-3.5 py-2.5"
+      "px-3.5 py-2.5 text-sm"
+    when :"sm-rounded"
+      "p-1"
+    when :"md-rounded"
+      "p-1.5"
+    when :"lg-rounded"
+      "p-2"
     end
-    button_classes = tw(button_classes, variant_classes, options[:class])
+
+    base_classes_with_color = base_classes.sub("{{bg}}", "{{color}}")
+    hover_classes = base_classes.sub("{{bg}}", "{{hover_color}}")
+
+    button_classes = tw(base_classes_with_color, variant_classes, options[:class], button_classes)
+    hover_classes = tw(hover_classes, variant_classes, options[:class], button_classes)
 
     text = label if label.present?
     text = capture(&block) if block
     render "components/ui/button", text:, button_classes:, as:, href:, data:, **options
+  end
+
+  def render_primary_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
+    render_button(label, text:, variant:, as:, href:, data:, button_classes: "rounded bg-primary-600 hover:bg-primary-500", **options, &block)
   end
 
   def render_secondary_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
-    button_classes = "rounded bg-white px-2 py-1 text-xs font-semibold text-secondary-900 shadow-sm ring-1 ring-inset ring-secondary-300 hover:bg-secondary-50"
-    variant_classes = case variant.to_sym
-    when :lg
-      "px-2.5 py-1.5"
-    when :sm
-      "px-2 py-1 text-xs"
-    when :md
-      "rounded-md px-2 py-1"
-    when :xl
-      "rounded-md px-3 py-2"
-    when :xxl
-      "rounded-md px-3.5 py-2.5"
-    end
-    button_classes = tw(button_classes, variant_classes, options[:class])
-
-    text = label if label.present?
-    text = capture(&block) if block
-    render "components/ui/button", text:, button_classes:, as:, href:, data:, **options
+    render_button(label, text:, variant:, as:, href:, data:, button_classes: "rounded bg-white text-xs font-semibold text-secondary-900 shadow-sm ring-1 ring-inset ring-secondary-300 hover:bg-secondary-50", **options, &block)
   end
 
   def render_primary_rounded_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
-    button_classes = "rounded-full bg-primary-600 text-xs font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-    variant_classes = case variant.to_sym
-    when :lg
-      "px-2.5 py-1.5"
-    when :sm
-      "px-2 py-1 text-xs"
-    when :md
-      "px-2 py-1"
-    when :xl
-      "px-3 py-2"
-    when :xxl
-      "px-3.5 py-2.5"
-    end
-    button_classes = tw(button_classes, variant_classes, options[:class])
-
-    text = label if label.present?
-    text = capture(&block) if block
-    render "components/ui/button", text:, button_classes:, as:, href:, data:, **options
+    render_button(label, text:, variant:, as:, href:, data:, button_classes: "rounded-full bg-primary-600 hover:bg-primary-500 text-xs font-semibold text-white shadow-sm", **options, &block)
   end
 
   def render_secondary_rounded_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
-    button_classes = "rounded-full bg-secondary-600 text-xs font-semibold text-white shadow-sm hover:bg-secondary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-600"
-    variant_classes = case variant.to_sym
-    when :lg
-      "px-2.5 py-1.5"
-    when :sm
-      "px-2 py-1 text-xs"
-    when :md
-      "px-2 py-1"
-    when :xl
-      "px-3 py-2"
-    when :xxl
-      "px-3.5 py-2.5"
-    end
-    button_classes = tw(button_classes, variant_classes, options[:class])
-
-    text = label if label.present?
-    text = capture(&block) if block
-    render "components/ui/button", text:, button_classes:, as:, href:, data:, **options
+    render_button(label, text: text, variant:, as:, href:, data:, button_classes: "rounded-full bg-secondary-600 text-xs font-semibold text-white shadow-sm hover:bg-secondary-500", **options, &block)
   end
 
   def render_primary_leading_icon_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
     optional = {}
-    button_classes = "inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-    variant_classes = case variant.to_sym
-    when :lg
-      "px-2.5 py-1.5"
-    when :sm
-      "px-2 py-1 text-xs"
-    when :md
-      "px-2 py-1"
-    when :xl
-      "px-3 py-2"
-    when :xxl
-      "px-3.5 py-2.5"
-    end
-    button_classes = tw(button_classes, variant_classes, options[:class])
-
-    text = label if label.present?
-    text = capture(&block) if block
     optional[:label] = label
-    render "components/ui/button", text:, button_classes:, as:, href:, data:, optional:, **options
-  end  
+    render_button(label, text:, variant:, as:, href:, data:, button_classes: "inline-flex items-center gap-x-1.5 rounded-md bg-primary-600 hover:bg-primary-500", optional:, **options, &block)
+  end
+
+  def render_primary_trailing_icon_button(label = "", text: nil, variant: :lg, as: :button, href: nil, data: {}, **options, &block)
+    optional = {}
+    optional[:label] = label
+    optional[:trailing] = true
+    render_button(label, text:, variant:, as:, href:, data:, button_classes: "inline-flex items-center gap-x-1.5 rounded-md bg-primary-600 hover:bg-primary-500", optional:, **options, &block)
+  end
+  
+  def render_circular_icon_button(label = "", text: nil, variant: :"sm-rounded", as: :button, href: nil, data: {}, **options, &block)
+    optional = {}
+    optional[:rounded] = true
+    render_button(label, text:, variant:, as:, href:, data:, button_classes: "rounded-full bg-primary-600 text-white shadow-sm hover:bg-primary-500 focus-visible:outline-offset-2 focus-visible:outline-primary-600", optional:, **options, &block)
+  end   
 end
--- test/components/previews/button_preview.rb
+++ test/components/previews/button_preview.rb
@@ -63,4 +63,30 @@ class ButtonPreview < Lookbook::Preview
       inline_svg_tag "heroicons-users.svg", class: "-ml-0.5 h-5 w-5"
     end
   end
+
+  # Primary Icon Trailing Button
+  # ----------------
+  # This example uses dynamic preview parameters
+  # which can be edited live in the Lookbook UI
+  #
+  # @param text
+  # @param variant select [sm, md, lg, xl, xxl]
+  def primary_trailing_icon(text: "Speichern", variant: "lg")
+    render_primary_trailing_icon_button text, variant: variant do
+      inline_svg_tag "heroicons-users.svg", class: "-ml-0.5 h-5 w-5"
+    end
+  end
+  
+  # Circular Icon Button
+  # ----------------
+  # This example uses dynamic preview parameters
+  # which can be edited live in the Lookbook UI
+  #
+  # @param text
+  # @param variant select [sm-rounded, md-rounded, lg-rounded]
+  def circular_icon(text: "", variant: "sm-rounded")
+    render_circular_icon_button text, variant: variant do
+      inline_svg_tag "heroicons-plus.svg", class: "h-5 w-5"
+    end
+  end   
 end
