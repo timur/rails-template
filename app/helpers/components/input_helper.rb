19c6a69181aa02db9080d05aefc45e534bbdf652
Timur <tiyalcin@gmail.com>
2023-10-16 11:15:59 +0200

    add container classes

--- app/helpers/components/input_helper.rb
+++ app/helpers/components/input_helper.rb
@@ -1,7 +1,10 @@
 module Components::InputHelper
-  def render_input(name:, label: false, id: nil, type: :text, value: nil, **options)
+  def render_input(name:, label: false, id: nil, type: :text, value: nil, width_class: nil, **options)
     base_classes = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
     input_classes = tw(base_classes, options[:class])
+    base_container_classes = "mt-2"
+    container_icon_classes = "relative rounded-md shadow-sm"
+    container_classes = tw(base_container_classes, has_icon?(options) ? container_icon_classes : "", width_class)
 
     options.reverse_merge!(
       label: (options[:label] || false),
@@ -13,7 +16,8 @@ module Components::InputHelper
       autocapitalize: (options[:autocapitalize] || nil),
       autocorrect: (options[:autocorrect] || nil),
       autofocus: (options[:autofocus] || nil),
-      hint: (options[:hint] || "")
+      hint: (options[:hint] || ""),
+      width_class: (options[:width_class] || ""),
     )
     render partial: "components/ui/input", locals: {
       type:,
@@ -22,7 +26,15 @@ module Components::InputHelper
       value:,
       id:,
       input_classes:,
+      container_classes:,
       options: options
     }
+  end
+
+
+  private
+
+  def has_icon? options
+    (options[:has_error] || options[:trailing_text] || options[:leading_text])
   end
 end
