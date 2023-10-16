3bc41fde29ed26c6a6b3ff13560d19831a8ac555
Timur <tiyalcin@gmail.com>
2023-10-16 11:15:43 +0200

    add salary for testing

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -1,6 +1,7 @@
 class UserForm < ApplicationForm
   attribute :firstname, :string
   attribute :lastname, :string
+  attribute :salary, :decimal
   validates :firstname, presence: true
   validates :lastname, presence: true
 
