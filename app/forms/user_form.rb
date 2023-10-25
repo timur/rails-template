5968300194fca910324e445aa1839ff692868a43
Timur <tiyalcin@gmail.com>
2023-10-25 07:16:00 +0200

    display error messages in check box

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -7,11 +7,15 @@ class UserForm < ApplicationForm
   attribute :check_box_test, :string
   attribute :check_box_test2, :string
   attribute :check_box_test3, :string
+  attribute :accept, :boolean
+  attribute :accept2, :boolean, default: false
   attribute :tags, CustomArrayType.new
   
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :framework, presence: true
+  validates :accept2, acceptance: { accept: true, message: 'must be accepted 2' }
+  validates :accept, presence: { message: "Custom error message accept me." }
   validates :salary, comparison: { less_than: 1000 }
 
   TAGS = %i[wifi animals parking sea_view]
--- app/helpers/components/checkbox_helper.rb
+++ app/helpers/components/checkbox_helper.rb
@@ -1,7 +1,10 @@
 module Components::CheckboxHelper
   def render_check_box(name:, id:, value:, label:, checkbox_value:, **options)
+    base_classes = "h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600"
+    input_classes = tw(base_classes, options[:class])
     partial = "components/ui/check_box"
     partial = "components/ui/check_box_array" if options[:array]
+    Rails.logger.info "name #{name} value: #{value} checkbox_value #{checkbox_value} checked: #{value === checkbox_value}"
     options.reverse_merge!(
       required: (options[:required] || false),
       disabled: (options[:disabled] || false),
@@ -12,6 +15,7 @@ module Components::CheckboxHelper
       value:,
       id:,
       label:,
+      input_classes:,
       checked: value === checkbox_value,
       checkbox_value:,
       options: options
