fb6500d5628a744e75df528d0c503cf6e53c8264
Timur <tiyalcin@gmail.com>
2023-10-25 17:32:14 +0200

    new buttons

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,7 @@
 class CalendarController < ApplicationController
 
   def index
-    @date = params[:date] || Date.today
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @start_date = @date.beginning_of_month.beginning_of_week(:monday)
     @end_date = @date.end_of_month.end_of_week(:monday)
     @date_range = (@start_date..@end_date).to_a
