3f2f17d33901826f7981701913c5295fcf19e53b
Timur <tiyalcin@gmail.com>
2023-10-26 10:06:23 +0200

    move stuff into helper

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,25 +1,29 @@
 require "tailwind_merge"
 
 module CalendarHelper
+
+  def compact_month_calendar(date = Date.today, navigation = true)
+    render partial: "calendar/compact_month_calendar", locals: {
+      date:,
+      navigation:
+    }
+  end
+
   def year_view_classes(calendar_date, date)
-    merger = TailwindMerge::Merger.new
-    classes = ""
-    base_classes = "py-1.5 hover:bg-gray-100 focus:z-10"
+    classes = %w(py-1.5 hover:bg-gray-100 focus:z-10)
     # is in month
-    classes = merger.merge("#{base_classes} bg-white text-gray-900") if month_range(calendar_date).cover?(date)
-    classes = merger.merge("#{base_classes} #{classes} bg-gray-50 text-gray-400") unless month_range(calendar_date).cover?(date)
-    classes = merger.merge("#{classes} rounded-tl-lg") if whole_range(calendar_date).to_a.first === date
-    classes = merger.merge("#{classes} rounded-br-lg") if whole_range(calendar_date).to_a.last === date
-    classes = merger.merge("#{classes} rounded-tr-lg") if last_day_week(calendar_date.beginning_of_month) === date
-    classes = merger.merge("#{classes} rounded-bl-lg") if first_day_week(calendar_date.end_of_month) === date
+    classes.push(%w(bg-white text-gray-900)) if month_range(calendar_date).cover?(date)
+    classes.push(%w(bg-gray-50 text-gray-400)) unless month_range(calendar_date).cover?(date)
+    classes.push(%w(rounded-tl-lg)) if whole_range(calendar_date).to_a.first === date
+    classes.push(%w(rounded-br-lg)) if whole_range(calendar_date).to_a.last === date
+    classes.push(%w(rounded-tr-lg)) if last_day_week(calendar_date.beginning_of_month) === date
+    classes.push(%w(rounded-bl-lg)) if first_day_week(calendar_date.end_of_month) === date
     classes
   end
 
   def year_view_time_classes(calendar_date, date)
-    merger = TailwindMerge::Merger.new
-    classes = ""
-    base_classes = "mx-auto flex h-7 w-7 items-center justify-center rounded-full"
-    classes = merger.merge("#{base_classes} bg-indigo-600 font-semibold text-white") if date === Date.today
+    classes = %w(mx-auto flex h-7 w-7 items-center justify-center rounded-full)
+    classes.push(%w(bg-indigo-600 font-semibold text-white)) if date === Date.today
     classes
   end
 
@@ -37,5 +41,9 @@ module CalendarHelper
 
   def first_day_week date
     date.beginning_of_week(:monday)
+  end
+
+  def date_range(date)
+    (date.beginning_of_month.beginning_of_week(:monday)..date.end_of_month.end_of_week(:monday)).to_a
   end
 end
