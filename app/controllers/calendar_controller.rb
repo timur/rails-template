ac93cebf8fc2232b90fb0a886cb9361047f3849e
Timur <tiyalcin@gmail.com>
2023-10-31 10:31:22 +0100

    update params

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,8 +1,15 @@
 class CalendarController < ApplicationController
 
   def index
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @date2 = params[:date2] ? Date.parse(params[:date2]) : Date.today
+    id = params[:id] ? params[:id] : "calendar"
+    date = params[:date] ? Date.parse(params[:date]) : Date.today
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream { 
+        render turbo_stream: 
+          turbo_stream.update(id, partial: "shared/month_calendar", locals: { date: date, id: id}) }
+    end
   end
 
   def year
