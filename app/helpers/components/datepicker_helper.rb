8c2c2eca966a12cff5b02ccead98c27dae7f3cf1
Timur Yalcin <timur@yalcin.digital>
2023-12-27 05:28:20 +0100

    remove unused options

--- app/helpers/components/datepicker_helper.rb
+++ app/helpers/components/datepicker_helper.rb
@@ -1,21 +1,20 @@
 module Components::DatepickerHelper
-  def render_datepicker(name:, label: false, id: nil, type: :text, value: nil, width_class: nil, template: "components/ui/datepicker", **options)
+  def render_datepicker(name:, id: nil, value: nil, template: "components/ui/datepicker", **options)
     base_classes = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6 pr-10"
     input_classes = "#{base_classes} #{options[:class]}"
     container_classes = "relative rounded-md shadow-sm"
+    type = "text"
 
     options.reverse_merge!(
-      label: (options[:label] || false),
       required: (options[:required] || false),
       disabled: (options[:disabled] || false),
       readonly: (options[:readonly] || false),
       placeholder: (options[:placeholder] || ""),
       hint: (options[:hint] || ""),
-      width_class: (options[:width_class] || ""),
+      width_class: (options[:width_class] || "")
     )
     render partial: template, locals: {
       type:,
-      label:,
       name:,
       value:,
       id:,
