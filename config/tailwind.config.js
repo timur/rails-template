1622302048e1c198204700b6c60b9ac038e5ceed
Timur Yalcin <timur@yalcin.digital>
2024-01-02 11:17:32 +0100

    hero section complete

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        //heading: ["Barcelony", 'sans-serif'],
       },
     },
   },
