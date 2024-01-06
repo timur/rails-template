52fd8c850983b233c0fd1085fb66e8d2a9b7bbad
Timur Yalcin <timur@yalcin.digital>
2024-01-06 10:07:56 +0100

    refactor calendar tailwind classes handling

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -29,41 +29,49 @@ module CalendarHelper
     }
   end
 
-  def year_view_classes(calendar_date, date)
-    classes = %w(py-1.5 hover:bg-gray-100 focus:z-10)
-    # is in month
-    classes.push(%w(bg-white text-gray-900)) if month_range(calendar_date).cover?(date)
-    classes.push(%w(bg-gray-50 text-gray-400)) unless month_range(calendar_date).cover?(date)
-    classes.push(%w(rounded-tl-lg)) if whole_range(calendar_date).to_a.first === date
-    classes.push(%w(rounded-br-lg)) if whole_range(calendar_date).to_a.last === date
-    classes.push(%w(rounded-tr-lg)) if last_day_week?(calendar_date.beginning_of_month) === date
-    classes.push(%w(rounded-bl-lg)) if first_day_week?(calendar_date.end_of_month) === date
-    classes
+  def calendar_cell_classes(date, day)
+    TailwindClasses.calendar_cell.render(
+      in_current_month: is_in_current_month?(day),
+      is_first_in_month_range: first_day_of_month_range(whole_month_range(date), day),
+      is_last_in_month_range: last_day_of_month_range(whole_month_range(date), day),
+      is_last_day_of_first_week: last_day_of_first_week(date, day),
+      is_first_day_of_last_week: first_day_of_last_week(date, day)
+    )
   end
 
-  def year_view_time_classes(calendar_date, date)
-    classes = %w(mx-auto flex h-7 w-7 items-center justify-center rounded-full)
-    classes.push(%w(bg-indigo-600 font-semibold text-white)) if date === Date.today
-    classes
-  end
-  
-  def month_range date
-    date.beginning_of_month..date.end_of_month
+  def calendar_cell_inside_classes(date, day)
+    TailwindClasses.calendar_cell_inside.render(
+      today: is_today?(day)
+    )
+  end  
+
+  def is_today?(day)
+    Date.today === day
   end
 
-  def whole_range date
+  def is_in_current_month?(day)
+    Date.today.all_month.cover? day
+  end
+
+  # Returns a range of dates for the beginning of the week of a month and end of a week for the month
+  # for example 25.10.2023 - 05.11.2023 this is for November of 2023
+  def whole_month_range(date)
     date.beginning_of_month.beginning_of_week(:monday)..date.end_of_month.end_of_week(:monday)
+  end  
+
+  def first_day_of_month_range(range, day)
+    range.first === day
   end
 
-  def last_day_week? date
-    date.end_of_week(:monday)
+  def last_day_of_first_week(date, day)
+    date.beginning_of_month.end_of_week(:monday) === day
   end
 
-  def first_day_week? date
-    date.beginning_of_week(:monday)
+  def first_day_of_last_week(date, day)
+    date.end_of_month.beginning_of_week(:monday) === day
   end
 
-  def date_range(date)
-    (date.beginning_of_month.beginning_of_week(:monday)..date.end_of_month.end_of_week(:monday)).to_a
+  def last_day_of_month_range(range, day)
+    range.last === day
   end
 end
---
+++ test/helpers/calendar_helper_test.rb
@@ -0,0 +1,74 @@
+require 'test_helper'
+
+class CalendarHelperTest < ActionView::TestCase
+  include CalendarHelper
+
+  test '#compact_month_calendar tests if the correct partial is rendered' do
+    date = Date.today
+    render partial: 'calendar/compact_month_calendar', locals: { date:, navigation: false}
+    assert_select 'div.font-semibold', text: "#{I18n.l(date, format: "%B")} #{date.year}"
+  end
+
+  test '#first_day_of_month_range tests if the 15. of a month is the first date of a month range' do
+    month_date = Date.today
+    day = Date.new(month_date.year, month_date.month, 15)
+    range = whole_month_range(month_date)
+    assert_not first_day_of_month_range(range, day)
+  end
+
+  test '#first_day_of_month_range tests if a specific date is the first date of a month range' do
+    # use December 2023
+    month_date = Date.new(2023, 12, 1)
+    # 27.11.2023 is the first day of the week in the first week of December
+    day = Date.new(month_date.year, 11, 27)
+    range = whole_month_range(month_date)
+    assert first_day_of_month_range(range, day)
+  end
+
+  test '#last_day_of_first_week tests if a date is the last day of the first month week' do
+    month_date = Date.today
+    day = Date.new(month_date.year, month_date.month, 15)
+    assert_not last_day_of_first_week(month_date, day)
+  end
+
+  test '#last_day_of_first_week tests if a date is the last day of the first month week and should return true' do
+    # use December 2023
+    month_date = Date.new(2023, 12, 1)
+    # 03.12.2023 is the last day of the week in the first week of December
+    day = Date.new(month_date.year, 12, 3)
+    assert last_day_of_first_week(month_date, day)
+  end
+
+  test '#first_day_of_last_week tests if a date is the first day of the first month week' do
+    # use December 2023
+    month_date = Date.new(2023, 12, 1)
+    # 27.11.2023 is the first day of the week in the first week of December
+    day = Date.new(month_date.year, 11, 27)
+    assert_not first_day_of_last_week(month_date, day)
+  end
+
+  test '#first_day_of_last_week tests if a date is the first day of the first month week and should return true' do
+    # use December 2023
+    month_date = Date.new(2023, 12, 1)
+    # 27.11.2023 is the first day of the week in the first week of December
+    day = Date.new(month_date.year, 12, 25)
+    assert first_day_of_last_week(month_date, day)
+  end
+
+  test '#last_day_of_month_range tests if the 15. of a month is the first date of a month range' do
+    month_date = Date.today
+    day = Date.new(month_date.year, month_date.month, 15)
+    range = whole_month_range(month_date)
+    assert_not last_day_of_month_range(range, day)
+  end
+
+  test '#last_day_of_month_range tests if a specific date is the first date of a month range' do
+    # use January 2024
+    month_date = Date.new(2024, 1, 1)
+    # 04.02.2024 is the last day of the week in the last week of January
+    day = Date.new(month_date.year, 2, 4)
+    range = whole_month_range(month_date)
+    assert last_day_of_month_range(range, day)
+  end  
+  
+end
