66995614216d6f7a5d815f1c59f9a2b84127ff3e
Timur Yalcin <timur@yalcin.digital>
2024-01-08 19:49:21 +0100

    fix a bug in current month

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,6 +1,7 @@
 module CalendarHelper
 
   def compact_month_calendar(options = {})
+    puts "options: #{options}"
     date = options[:date] || Date.today
     navigation = options.key?(:navigation) ? options[:navigation] : true
     param = options[:param] || :date
@@ -33,7 +34,7 @@ module CalendarHelper
 
   def calendar_cell_classes(date, day)
     TailwindClasses.calendar_cell.render(
-      in_current_month: is_in_current_month?(day),
+      in_current_month: is_in_month?(date, day),
       is_first_in_month_range: first_day_of_month_range(whole_month_range(date), day),
       is_last_in_month_range: last_day_of_month_range(whole_month_range(date), day),
       is_last_day_of_first_week: last_day_of_first_week(date, day),
@@ -75,8 +76,8 @@ module CalendarHelper
     Date.today === day
   end
 
-  def is_in_current_month?(day)
-    Date.today.all_month.cover? day
+  def is_in_month?(date, day)
+    date.all_month.cover? day
   end
 
   # Returns a range of dates for the beginning of the week of a month and end of a week for the month
