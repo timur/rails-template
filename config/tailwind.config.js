1d57272c640ad4edf6f47cd5932c17bbb3272db4
Timur <tiyalcin@gmail.com>
2024-01-26 12:27:06 +0100

    add animation stuff

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,6 +18,21 @@ module.exports = {
         secondary: colors.green,
         neutral: colors.gray,
       },
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+      },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
