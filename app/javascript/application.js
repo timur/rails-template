be6d3e4eb052bdee1674fd39949eac5456da4abd
Timur Yalcin <timur@yalcin.digital>
2024-02-08 23:03:18 +0100

    restore stuff

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,32 @@
 import "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage";
+import Alpine from "alpinejs";
+import ui from "@alpinejs/ui";
+import focus from "@alpinejs/focus";
+import mask from "@alpinejs/mask";
+import LocalTime from "local-time";
+
 import "./controllers"
 
-addEventListener("turbo:before-fetch-request", (event) => {
-  const headers = event.detail.fetchOptions.headers;
-  console.log("before-fetch-request", headers);
-});
+Alpine.plugin(ui);
+Alpine.plugin(focus);
+Alpine.plugin(mask);
 
-addEventListener("turbo:frame-missing", (event) => {
-  const { detail: { response, visit } } = event;
-  console.log("frame-missing", response, visit);
-});
+ActiveStorage.start();
+window.Alpine = Alpine;
+Alpine.start();
+
+LocalTime.start();
+
+Turbo.setConfirmMethod((message, element) => {
+  const data = JSON.parse(`${message}`);
+  Alpine.store('confirmModal').toggle();
+  Alpine.store('confirmModal').title = data.title;
+  Alpine.store('confirmModal').message = data.message;
+
+  return new Promise((resolve, reject) => {
+    window.addEventListener("confirm", () => {
+      resolve(true);
+    }, { once: true })
+  })
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,13 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "alpinejs" # @3.13.5
+pin "stimulus-use" # @0.52.2
+pin "@alpinejs/ui", to: "@alpinejs--ui.js" # @3.13.5
+pin "@alpinejs/focus", to: "@alpinejs--focus.js" # @3.13.5
+pin "@alpinejs/mask", to: "@alpinejs--mask.js" # @3.13.5
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.1.3
+pin "local-time" # @3.0.2
