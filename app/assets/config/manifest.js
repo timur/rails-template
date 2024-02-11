3584c87f2c855598fe5432ec044bf54da4e7c7b4
Timur Yalcin <timur@yalcin.digital>
2024-02-11 12:06:50 +0100

    add fonts from lightgallery

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_tree ../svg
+//= link_tree ../fonts
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
