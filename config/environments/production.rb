369e468cb93510ef3b822e5015dabfa2ce8a61cd
Timur Yalcin <timur@yalcin.digital>
2024-03-21 16:20:31 +0100

    looks like it has to be deactived

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,13 +43,14 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://ruby-rails.net', 'https://template.ruby-rails.net', %r{https://template.*}]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
