0f1fcf9920cf77ea222083c89ad6fe8d810f4478
Timur Yalcin <timur@yalcin.digital>
2024-03-22 08:58:31 +0100

    move checkboxes

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  submit() {
+  submit(event) {
+    this.element.requestSubmit();
+  }
+
+  checkbox(event) {
+    event.stopPropagation();
     this.element.requestSubmit();
   }
 }
