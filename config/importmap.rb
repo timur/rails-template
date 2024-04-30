73fd00515c35fdef3f47ef9cbb93653a484fbf79
Timur <tiyalcin@gmail.com>
2024-04-30 12:19:00 +0200

    update frontend libs

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,11 +4,11 @@ pin "landing", preload: false
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: false
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js", preload: false # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: false
-pin "alpinejs" # @3.13.7
+pin "alpinejs" # @3.13.10
 pin "stimulus-use", preload: false # @0.52.2
-pin "@alpinejs/ui", to: "@alpinejs--ui.js", preload: false # @3.13.7
-pin "@alpinejs/focus", to: "@alpinejs--focus.js", preload: false # @3.13.7
-pin "@alpinejs/mask", to: "@alpinejs--mask.js", preload: false # @3.13.7
+pin "@alpinejs/ui", to: "@alpinejs--ui.js" # @3.13.10
+pin "@alpinejs/focus", to: "@alpinejs--focus.js" # @3.13.10
+pin "@alpinejs/mask", to: "@alpinejs--mask.js" # @3.13.10
 pin "@rails/activestorage", to: "@rails--activestorage.js", preload: false # @7.1.3
 pin "local-time", preload: false # @3.0.2
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: false # @2.11.8
@@ -21,7 +21,7 @@ pin "load-images", preload: false
 pin "preload-images", preload: false
 pin "ahoy", preload: false # @1.0.1
 pin "os", preload: false # @2.0.1
-pin "apexcharts", preload: false # @3.48.0
+pin "apexcharts" # @3.49.0
 pin "lg-thumbnail", preload: false
 pin "@shopify/draggable", to: "@shopify--draggable.js", preload: false # @1.1.3
 pin_all_from "app/javascript/controllers", under: "controllers", preload: false
