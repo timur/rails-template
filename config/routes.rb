9b38c93672d1132b6c95bb7cae3b07a2fec63a00
Timur Yalcin <timur@yalcin.digital>
2024-06-25 14:19:47 +0200

    add empty lines

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
   get  "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  
   resources :sessions, only: [:index, :show, :destroy]
+  
   resource  :password, only: [:edit, :update]
+  
   namespace :identity do
     resource :email,              only: [:edit, :update]
     resource :email_verification, only: [:show, :create]
